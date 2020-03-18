7973fb8cf8a6f71f7488b22270b2c94d1c86eea5
TwistPlay <twistplay@mail.ru>
2020-03-18 18:10:55 +0300

    Add files via upload

---
+++ commands/pic/secret2.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const { get } = require("snekfetch");
+const client = require('nekos.life');
+const neko = new client();
+
+
+module.exports.run = async (bot, message, args, ops, async, run) => {
+    if (!message.channel.nsfw) return message.reply("🔞 Это не NSFW канал, чтобы использовать такие команды.");
+
+    const msg = await message.channel.send(`:thinking: **${message.member.displayName}** Ищет себе лольку...`);
+    const { body } = await get("https://nekos.life/api/v2/img/anal");
+    await msg.edit({
+      embed: {
+        "title": "Тыкни сюда, если не прогрузилась пикча.",
+        "url": body.url,
+        "color": 6192321,
+        "image": {
+        "url": body.url
+        },
+  }
+    })
+};
+module.exports.help = {
+    name: "anal"
+}
