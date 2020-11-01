e96923585429529bfe40f0a8cdca75a691fc72e2
Anatoly <twistplay@mail.ru>
2020-11-01 17:10:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,70 +1,68 @@
-const Discord = require("discord.js")
-const config = require("./config.json")
-const db = require("quick.db")
-const { addexp } = require("./commands/xp.js")
-const bot = new Discord.Client();
+const Discord = require("discord.js");
+const client = new Discord.Client();
 const fs = require("fs");
-bot.commands = new Discord.Collection();
-const ownerID = '473526373575819264';
+const snekfetch = require('snekfetch');
 
-const active = new Map();
-
-const serverStats = {
-    memberCoutID: '702401815815127061'
+client.db = require("quick.db");
+client.commands = new Discord.Collection();
+client.cooldown = new Discord.Collection();
+client.config = {
+    TOKEN: "token",
+    prefix: "!"
 };
 
-fs.readdir("./commands", (err, files) => { // Подключение команд 
-
-  if(err) console.log(err);
-
-  let jsfile = files.filter(f => f.split(".").pop() === "js"); // Подключение команд 
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-
-jsfile.forEach((f, i) =>{
-  let props = require(`./commands/${f}`); // основных команд
-  console.log(`${f} Загружен!`);
-  bot.commands.set(props.help.name, props);
-});
+// Load Commands
+fs.readdir("./commands/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(f => {
+        if (!f.endsWith(".js")) return;
+        let command = require(`./commands/${f}`);
+        client.commands.set(command.help.name, command);
+    });
 });
 
-fs.readdir("./commands/pic", (err, files) => { // Подключение изображений
-
-  if(err) console.log(err);
-
-  let jsfile = files.filter(f => f.split(".").pop() === "js"); // Подключение команд 
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-
-jsfile.forEach((f, i) =>{
-  let props = require(`./commands/pic/${f}`); //
-  console.log(`${f} Изображение загружена!`);
-  bot.commands.set(props.help.name, props);
-});
-});
-bot.on('ready', () => {
-    bot.user.setActivity("-tw1st-", {
-    type: "STREAMING",
-    url: "https://www.twitch.tv/twistplay2"
-});
-});
-  
-bot.on("message", async message => {
-
-  let prefix = config.prefix;
-  if (!message.content.startsWith(config.prefix)) return;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
-  if(message.author.bot) return;
-  if(!message.guild) return;
-return addexp(message)
+fs.readdir("./commands/pic", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(f => {
+        if (!f.endsWith(".js")) return;
+        let command = require(`./commands/pic/${f}`);
+        client.commands.set(command.help.name, command);
+    });
 });
 
-bot.login(process.env.BOT_TOKEN)
+// Events
+client.once("ready", () => {
+    console.log("Ready!");
+});
+
+client.on("error", console.error);
+
+client.on("warn", console.warn);
+
+client.on("message", async (message) => {
+    if (!message.guild || message.author.bot) return;
+    // Handle XP
+    xp(message);
+    // command handler
+    if (!message.content.startsWith(client.config.prefix)) return;
+    let args = message.content.slice(client.config.prefix.length).trim().split(" ");
+    let command = args.shift().toLowerCase();
+    let commandFile = client.commands.get(command);
+    if (!commandFile) return;
+    commandFile.run(client, message, args);
+});
+
+function xp(message) {
+    if (!client.cooldown.has(`${message.author.id}`) || !(Date.now() - client.cooldown.get(`${message.author.id}`) > client.config.cooldown)) {
+        let xp = client.db.add(`xp_${message.author.id}`, 1);
+        let level = Math.floor(0.3 * Math.sqrt(xp));
+        let lvl = client.db.get(`level_${message.author.id}`) || client.db.set(`level_${message.author.id}`,1);;
+        if (level > lvl) {
+            let newLevel = client.db.set(`level_${message.author.id}`,level);
+            message.channel.send(`:tada: ${message.author.toString()}, ты получил новый уровень: ${newLevel}!`);
+        }
+        client.cooldown.set(`${message.author.id}`, Date.now());
+    }
+}
+
+client.login(process.env.client.config.TOKEN);
